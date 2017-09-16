571a2ebe6437b57dd1461d09ad285ecd8859ff29
czhwu <czhwu@qq.com>
2017-09-16 20:31:31 +0800

    first commit

---
+++ cl/qz.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import os
+import re
+import requests
+
+
+def get_html_text(url):
+    pass
+
+
+def get_image_url(html):
+    pass
+
+
+def save_image(image_url):
+    pass
+
+
+url = ''
+html = get_html_text(url)
+image_url = get_image_url(html)
+save_image(image_url)
